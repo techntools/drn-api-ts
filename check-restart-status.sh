dde280bddcd3302329b20dc769c0c76cbffbe9aa
Santosh Bandichode <santoshg550@gmail.com>
2024-10-09 01:33:40 +0530

    Sleep in github actions

--- check-restart-status.sh
+++ check-restart-status.sh
@@ -2,19 +2,8 @@
 
 PROCESS_NAME="drn-apis"
 URL="http://localhost:8080/health-check"
-DELAY=3                                     # Delay in seconds before making the HTTP request
 EXPECTED_STATUS=200                         # The expected HTTP status code
 CURL_OUTPUT="curl_output.txt"               # Temporary file to store curl output
-
-pm2 startOrReload ecosystem.config.js
-if [ $? -ne 0 ]; then
-  echo "Failed to start process $PROCESS_NAME"
-  exit 1
-fi
-echo "Started process $PROCESS_NAME, waiting for $DELAY seconds..."
-
-# Wait for the specified delay
-sleep $DELAY
 
 echo "Making HTTP request to $URL..."
 curl -o /dev/null -s -w "%{http_code}" $URL > $CURL_OUTPUT
